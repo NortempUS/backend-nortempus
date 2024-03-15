db9a4a97536dedbac4bf38e0d08199093e2c7e1b
Marchabar <marchabar1@alum.us.es>
2024-03-15 20:03:56 +0100

    feat: urls

--- chat/urls.py
+++ chat/urls.py
@@ -4,6 +4,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("chat/<int:user_id>/", views.get_chats_by_user),
+    path("profile/chat/<int:user_id>/", views.get_chats_by_user),
     path("chat/<int:chat_id>/", views.get_chat_by_id),
 ]
