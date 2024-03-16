46a5404b20166da8bdbf771264f91644fad342ad
JMGarCas <jmgarciacasillas135@gmail.com>
2024-03-16 04:27:05 +0100

    feat: category names in list services

--- services/views.py
+++ services/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from category.models import Category
+
 from .models import Service
 from rest_framework.decorators import api_view, permission_classes
 
@@ -8,12 +10,19 @@ from rest_framework.permissions import IsAuthenticated
 from .serializers import ServiceSerializer
 
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def list_services(request):
+    category_names = []
     services = Service.objects.all()
+    for service in services:
+        category_names.append(service.category.category_type)
     serializer = ServiceSerializer(services, many=True)
 
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(
+        {"serializer": serializer.data, "category_names": category_names},
+        status=status.HTTP_200_OK,
+    )
